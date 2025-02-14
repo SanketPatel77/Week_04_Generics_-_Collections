f966f9ef3de94802279d4beaa3334855e1c294cc
SanketPatel77 <sanketpatel4872@gmail.com>
2025-02-14 23:36:51 +0530

    [Sanket] Add. Use of finally block.

---
+++ Day_04_Exception/src/main/java/exceptionhandling/finallyblockexecution/FinallyBlockExecution.java
@@ -0,0 +1,29 @@
+package exceptionhandling.finallyblockexecution;
+
+import java.util.Scanner;
+
+public class FinalBlockExecution {
+    public static void main(String[] args) {
+        // scanner obj
+        Scanner scanner = new Scanner(System.in);
+
+        //Take two integers from the user.
+        System.out.println("Enter 2 number");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        //Perform division.
+        try {
+            //Handle ArithmeticException (if dividing by zero).
+            int c = a / b;
+            System.out.println("answer is : "+c);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        // will run regardless of exception
+        finally {
+            System.out.println("Operation completed");
+        }
+    }
+}
