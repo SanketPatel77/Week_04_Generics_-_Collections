ae0554f5eafdb05a6efcd1501bfe563eadccbe15
SanketPatel77 <sanketpatel4872@gmail.com>
2025-02-15 22:45:11 +0530

    Delete Day_04 folder.

--- Day_04_Exception/src/main/java/exceptionhandling/banktransactionsystem/InsufficientBalanceException.java
+++
@@ -1,8 +0,0 @@
-package exceptionhandling.banktransactionsystem;
-
-// Custom Exception for Insufficient Balance
-public class InsufficientBalanceException extends Exception {
-    public InsufficientBalanceException(String message) {
-        super(message);
-    }
-}
--- Day_04_Exception/src/main/java/exceptionhandling/customexception/InvalidAgeException.java
+++
@@ -1,9 +0,0 @@
-package exceptionhandling.customexception;
-// InvalidAgeException class definition
-public class InvalidAgeException extends Exception {
-    // Parameterized constructor
-    InvalidAgeException(String message) {
-        // Passing message to constructor to exception class constructor
-        super(message);
-    }
-}
